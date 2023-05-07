c2585fd3854dc663c725fb07d76524e7fc5f08ed
ishitpdesai <ishitpdesai@gmail.com>
2023-05-07 17:11:08 +0530

    Report Added

--- Software/real-time-object-detection/real_time_object_detection.py
+++ Software/real-time-object-detection/real_time_object_detection.py
@@ -19,57 +19,58 @@ net = cv2.dnn.readNetFromCaffe('MobileNetSSD_deploy.prototxt.txt', 'MobileNetSSD
 print("[INFO] starting video stream...")
 
 #vs = VideoStream(src=0).start()
-vs = VideoStream(src="http://192.168.39.203:81/stream").start()
-#vs = cv2.VideoCapture("http://192.168.186.203:81/stream")
-time.sleep(5.0)
-fps = FPS().start()
+vs = VideoStream(src="http://192.168.133.203:81/stream").start()
+#vs = cv2.VideoCapture("http://192.168.39.203:81/stream")
+#time.sleep(5.0)
+#fps = FPS().start()
 
 def generate_frames():
 	while True:		
 		frame = vs.read()
-		frame = cv2.flip(frame, 0)
-		frame = imutils.resize(frame, width=400)
-		
-		(h, w) = frame.shape[:2]
-		blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
-			0.007843, (300, 300), 127.5)
+		if frame is not None:		
+			frame = cv2.flip(frame, 0)
+			frame = imutils.resize(frame, width=400)
+			
+			(h, w) = frame.shape[:2]
+			blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
+				0.007843, (300, 300), 127.5)
 
-		net.setInput(blob)
-		detections = net.forward()
-		
-		for i in np.arange(0, detections.shape[2]):
-			confidence = detections[0, 0, i, 2]
+			net.setInput(blob)
+			detections = net.forward()
+			
+			for i in np.arange(0, detections.shape[2]):
+				confidence = detections[0, 0, i, 2]
 
-			if confidence > 0.2:
-				idx = int(detections[0, 0, i, 1])
+				if confidence > 0.2:
+					idx = int(detections[0, 0, i, 1])
 
-				if(CLASSES[idx]=="person"):
-					box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-					(startX, startY, endX, endY) = box.astype("int")
-					
-					label = "{}: {:.2f}%".format(CLASSES[idx],
-						confidence * 100)
-					cv2.rectangle(frame, (startX, startY), (endX, endY),
-						COLORS[idx], 2)
-					y = startY - 15 if startY - 15 > 15 else startY + 15
-					cv2.putText(frame, label, (startX, y),
-						cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
-		
-		ret,buffer=cv2.imencode('.jpg',frame)
-		frame=buffer.tobytes()
-		
-		yield(b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-		
-		key = cv2.waitKey(1) & 0xFF
-		
-		if key == ord("q"):
-			break
-		
-		fps.update()
+					if(CLASSES[idx]=="person"):
+						box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+						(startX, startY, endX, endY) = box.astype("int")
+						
+						label = "{}: {:.2f}%".format(CLASSES[idx],
+							confidence * 100)
+						cv2.rectangle(frame, (startX, startY), (endX, endY),
+							COLORS[idx], 2)
+						y = startY - 15 if startY - 15 > 15 else startY + 15
+						cv2.putText(frame, label, (startX, y),
+							cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+			
+			ret,buffer=cv2.imencode('.jpg',frame)
+			frame=buffer.tobytes()
+			
+			yield(b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+			
+			key = cv2.waitKey(1) & 0xFF
+			
+			if key == ord("q"):
+				break
+			
+#			fps.update()
 	
-	fps.stop()
-	print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
-	print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+#	fps.stop()
+	#print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
+	#print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
 	
 	cv2.destroyAllWindows()
 	vs.stop()
@@ -83,5 +84,5 @@ def video():
     return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__=="__main__":
-    app.run(host='192.168.39.182', port=5000)
+    #app.run(host='192.168.39.182', port=5000)
     app.run(debug=False)
